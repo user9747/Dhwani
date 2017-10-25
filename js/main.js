ac5f15392572c3a0c9d6f6fa065f0a6b3ccd5130
Nandu kmg <nandukrishnan55555@gmail.com>
2017-10-25 21:22:43 +0530

    added logo

--- js/main.js
+++ js/main.js
@@ -1343,7 +1343,7 @@ function closeNav() {
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
